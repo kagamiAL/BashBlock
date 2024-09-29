d78a718e2bc9ec63b26be834b437c0d9e3bd6b59
Alan Bach <kagamibach@gmail.com>
2024-09-29 12:01:26 -0600

    Add helper methods for marking pixels for removal

--- src/structs/pixel.zig
+++ src/structs/pixel.zig
@@ -1,10 +1,21 @@
 pub const Pixel = struct {
     shape_colour: ?[3]u8 = null,
     current_colour: ?[3]u8 = null,
+    marked: bool = false,
 
     pub fn init(self: *Pixel) *Pixel {
         self.current_colour = null;
         self.shape_colour = null;
         return self;
+    }
+
+    pub fn resetTemp(self: *Pixel) void {
+        self.current_colour = null;
+        self.marked = false;
+    }
+
+    pub fn markForRemoval(self: *Pixel, color: [3]u8) void {
+        self.current_colour = color;
+        self.marked = true;
     }
 };
